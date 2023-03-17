27f69a29cb664419493ab10542b653811fe08b8e
SaiVenkat12 <gsaiv1001@gmail.com>
2023-03-17 20:28:20 +0530

    [Sai Venkat]- Added-Create Address Book Table

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -1,3 +1,7 @@
 ----UC1-Create Address Book Database----
 create database Address_Book_Service;
 use Address_Book_Service;
+
+----UC1-Create Address Book Table----
+create Table Address_Book
+(First_Name varchar(100), Last_Name varchar(100), Address varchar(300), City varchar(50), State varchar(300), Zip_Code int, Phone_Number bigint, Email_Id varchar(500));
